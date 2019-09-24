b303baaba9d97683b129b1354dcd7ff13b06654a
陈东 <dong.chen03@ucarinc.com>
2019-09-24 09:36:53 +0800

    添加发布首页修改

---
+++ src/main/java/life/majiang/community/community/service/QuestionService.java
@@ -0,0 +1,20 @@
+package life.majiang.community.community.service;
+
+import life.majiang.community.community.model.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Description
+ * @Author chendong <dong.chen03@ucarinc.com>
+ * @Version V1.0.0
+ * @Date 2019/9/23
+ */
+@Service
+public class QuestionService {
+
+    public List<Question> list() {
+        return null;
+    }
+}
